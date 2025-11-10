263a3140222b267424aa3c01e54061d01589a086
Ivo Popov <ivopopv@gmail.com>
2025-11-10 16:57:29 +0200

    added basic CRUD to PostRestController

--- src/main/java/com/example/forum/controllers/rest/PostsRestController.java
+++ src/main/java/com/example/forum/controllers/rest/PostsRestController.java
@@ -1,9 +1,64 @@
 package com.example.forum.controllers.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.forum.exceptions.EntityNotFoundException;
+import com.example.forum.helpers.AuthenticationHelper;
+import com.example.forum.helpers.PostMapper;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("api/posts")
 public class PostsRestController {
+    private final PostsService postsService;
+    private final AuthenticationHelper authenticationHelper;
+    private final PostMapper postMapper;
+
+    public PostsRestController(PostsService postsService, AuthenticationHelper authenticationHelper, PostMapper postMapper) {
+        this.postsService = postsService;
+        this.authenticationHelper = authenticationHelper;
+        this.postMapper = postMapper;
+    }
+
+    @GetMapping
+    public List<PostDto> getAllPosts() {
+        return postsService.getAllPosts().stream()
+                .map(postMapper::toDto)
+                .toList();
+    }
+    @GetMapping("/{id}")
+    public PostDto getPostById(@PathVariable int id) {
+        try {
+            return postMapper.toDto(postsService.getPostById(id));
+        }catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+    @PostMapping
+    public PostDto createPost(@Valid @RequestBody PostDto postDto) {
+        //toDo authentication
+        Post post = postMapper.fromDto(postDto);
+        return postMapper.toDto(postsService.createPost(post));
+
+    }
+    @PutMapping("/{id}")
+    public PostDto updatePost(@PathVariable int id, @Valid @RequestBody PostDto postDto) {
+        //toDo authentication
+        Post post = postMapper.fromDto(postDto);
+        try {
+            return postMapper.toDto(postsService.updatePost(id, post));
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable int id) {
+        //toDo authentication
+        try {
+            postsService.deletePost(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/com/example/forum/helpers/PostMapper.java
@@ -0,0 +1,8 @@
+package com.example.forum.helpers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+    //toDo implement mapping when entities exist
+}
