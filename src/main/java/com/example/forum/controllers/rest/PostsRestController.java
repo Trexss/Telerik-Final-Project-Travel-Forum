417bb6ab01bb58e103a03d0fa53b8200aa2624a5
Ivo Popov <ivopopv@gmail.com>
2025-11-10 17:13:26 +0200

    added comment endpoints to Post Rest Controller

--- src/main/java/com/example/forum/controllers/rest/PostsRestController.java
+++ src/main/java/com/example/forum/controllers/rest/PostsRestController.java
@@ -2,6 +2,7 @@ package com.example.forum.controllers.rest;
 
 import com.example.forum.exceptions.EntityNotFoundException;
 import com.example.forum.helpers.AuthenticationHelper;
+import com.example.forum.helpers.CommentMapper;
 import com.example.forum.helpers.PostMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -14,11 +15,13 @@ public class PostsRestController {
     private final PostsService postsService;
     private final AuthenticationHelper authenticationHelper;
     private final PostMapper postMapper;
+    private final CommentMapper commentMapper;
 
-    public PostsRestController(PostsService postsService, AuthenticationHelper authenticationHelper, PostMapper postMapper) {
+    public PostsRestController(PostsService postsService, AuthenticationHelper authenticationHelper, PostMapper postMapper, CommentMapper commentMapper) {
         this.postsService = postsService;
         this.authenticationHelper = authenticationHelper;
         this.postMapper = postMapper;
+        this.commentMapper = commentMapper;
     }
 
     @GetMapping
@@ -45,6 +48,7 @@ public class PostsRestController {
     @PutMapping("/{id}")
     public PostDto updatePost(@PathVariable int id, @Valid @RequestBody PostDto postDto) {
         //toDo authentication
+        //toDo authorization only own posts
         Post post = postMapper.fromDto(postDto);
         try {
             return postMapper.toDto(postsService.updatePost(id, post));
@@ -55,10 +59,33 @@ public class PostsRestController {
     @DeleteMapping("/{id}")
     public void deletePost(@PathVariable int id) {
         //toDo authentication
+        //toDo authorization only own posts
         try {
             postsService.deletePost(id);
         } catch (EntityNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
+    @GetMapping("/{id}/comments")
+    public List<CommentDto> getCommentsByPostId(@PathVariable int id) {
+        //toDo authentication
+        try {
+            return postsService.getCommentsByPostId(id).stream()
+                    .map(commentMapper::toDto)
+                    .toList();
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+    @PostMapping("/{id}/comments")
+    public CommentDto addCommentToPost(@PathVariable int id, @Valid @RequestBody CommentDto commentDto) {
+        //toDo authentication
+        Comment comment = commentMapper.fromDto(commentDto);
+        try {
+            return commentMapper.toDto(postsService.addCommentToPost(id, comment));
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+    //toDo like other peoples Posts
 }
---
+++ src/main/java/com/example/forum/helpers/CommentMapper.java
@@ -0,0 +1,7 @@
+package com.example.forum.helpers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentMapper {
+}
